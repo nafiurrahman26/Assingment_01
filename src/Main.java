5034b18b5d655500f9f6052bc6fa774805e1217f
Nafiur Rahman Sabbir <nafiurrahmansabbir@gmail.com>
2023-04-21 18:16:08 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        Professor E1=new Professor(221, "Sabbir", 10000, "Computer Science");
+        System.out.println(E1.toString());
+        Administrator E2=new Administrator(222, "Auntu", 500, "GED");
+        System.out.println(E2.toString());
+        SupportStaff E3=new SupportStaff(223, "Bappi", 1000, "Suppoter");
+        System.out.println(E3.toString());
+        
+    }
+    
+}
